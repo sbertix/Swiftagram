f3ec96c732f1427555b466628478740dc1b4f707
sbertix <stefano.bertagno@gmail.com>
2020-03-06 18:12:45 +0100

    Refactor `Endpoint`

--- Sources/Swiftagram/Client/Endpoint.swift
+++ Sources/Swiftagram/Client/Endpoint.swift
@@ -12,24 +12,25 @@ import Foundation
 public struct Endpoint: Hashable {
     /// A `[String]` composed of all path components.
     internal var components: [String]
+    /// A `[String: String]` composed of all query components. Defaults to `[:]`
+    internal var queries: [String: String] = [:]
     /// A `[String: String]` composed of all custom header fields. Defaults to `[:]`.
-    internal var headerFields: [String: String]
+    internal var headerFields: [String: String] = [:]
 
     /// Compute the `URLRequest`.
     public var request: URLRequest? {
-        URL(string: components.joined(separator: "/"))
-            .flatMap { URLRequest(url: $0) }
+        var components = URLComponents(string: self.components.joined(separator: "/"))
+        components?.queryItems = queries.map { URLQueryItem(name: $0.key, value: $0.value) }
+        var request = components?.url.flatMap { URLRequest(url: $0) }
+        request?.allHTTPHeaderFields = headerFields
+        return request
     }
 
     // MARK: Lifecycle
     /// Init.
     /// - parameter components: A `Collection` of `String`s, forming a valid `https` address, when joined together using `/`.
     /// - parameter headerFields. A `Dictionary` of `(key: String, value: String)`, forming valid header fields.
-    public init(components: [String],
-                headerFields: [String: String] = [:]) {
-        self.components = components
-        self.headerFields = headerFields
-    }
+    public init(components: [String]) { self.components = components }
 
     // MARK: Composition
     /// An `Endpoint` pointing to `api/v1`.
@@ -40,13 +41,13 @@ public struct Endpoint: Hashable {
     public static var generic: Endpoint { return .init(components: ["https://www.instagram.com"]) }
 
     /// Append `item`.
-    public func appending<Item>(_ item: Item) -> Endpoint where Item: LosslessStringConvertible {
+    public func wrap<Item>(_ item: Item) -> Endpoint where Item: LosslessStringConvertible {
         var copy = self
         copy.components.append(String(item))
         return copy
     }
     /// Append `item`.
-    public func appending<Item>(_ item: Item) -> Endpoint where Item: CustomStringConvertible {
+    public func wrap<Item>(_ item: Item) -> Endpoint where Item: CustomStringConvertible {
         var copy = self
         copy.components.append(item.description)
         return copy
@@ -57,11 +58,24 @@ public struct Endpoint: Hashable {
         copy.components.append(component)
         return copy
     }
-
-    /// Append `headerFields`.
-    public func headerFields(_ headerFields: [String: String]) -> Endpoint {
+    
+    /// Append to `queries`.
+    public func query(key: String, value: String?) -> Endpoint {
         var copy = self
-        copy.headerFields = copy.headerFields.merging(headerFields, uniquingKeysWith: { _, rhs in rhs })
+        copy.queries[key] = value
+        return copy
+    }
+    /// Append `queries`. Empty `self.queries` if `nil`.
+    public func query(_ queries: [String: String]?) -> Endpoint {
+        var copy = self
+        copy.queries = queries.flatMap { copy.queries.merging($0) { _, rhs in rhs }} ?? [:]
+        return copy
+    }
+
+    /// Append `headerFields`. Empty `self.headerFields` if `nil`.
+    public func headerFields(_ headerFields: [String: String]?) -> Endpoint {
+        var copy = self
+        copy.headerFields = headerFields.flatMap { copy.headerFields.merging($0) { _, rhs in rhs }} ?? [:]
         return copy
     }
 }
