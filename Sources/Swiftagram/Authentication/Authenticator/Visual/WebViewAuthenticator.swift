e86ad690a370fa9e99fbf1e36bc554d775ed626d
sbertix <stefano.bertagno@gmail.com>
2020-03-09 11:13:49 +0100

    Refactor `WebViewAuthenticator`

--- Sources/Swiftagram/Authentication/Authenticator/Visual/WebViewAuthenticator.swift
+++ Sources/Swiftagram/Authentication/Authenticator/Visual/WebViewAuthenticator.swift
@@ -9,52 +9,6 @@
 import Foundation
 import WebKit
 
-@available(iOS 11, macOS 10.13, *)
-/// A `class` describing a self-navigating `WKWebView`.
-internal final class WebView: WKWebView, WKNavigationDelegate {
-    /// A block providing a `Secret`.
-    internal var onChange: ((Result<Secret, Swift.Error>) -> Void)?
-
-    /// A method called everytime a new page has finished loading.
-    internal func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        switch webView.url?.absoluteString {
-        case "https://www.instagram.com/"?:
-            // Check the `WKWebView`.
-            webView.configuration.websiteDataStore.httpCookieStore.getAllCookies { [self] in // keep alive.
-                // Obtain cookies.
-                let cookies = $0.filter {
-                    $0.domain.contains(".instagram.com") && ["ds_user_id", "sessionid", "csrftoken"].contains($0.name)
-                }.sorted { $0.name < $1.name }
-                // Prepare `Secret`.
-                guard cookies.count == 3 else {
-                    self.onChange?(.failure(AuthenticatorError.invalidCookies))
-                    return
-                }
-                self.onChange?(.success(.init(identifier: cookies[1],
-                                         crossSiteRequestForgery: cookies[0],
-                                         session: cookies[2])))
-            }
-            // No need to check anymore.
-            webView.navigationDelegate = nil
-        default:
-            webView.configuration.websiteDataStore.httpCookieStore.getAllCookies { [self] in // keep alive.
-                // Obtain cookies.
-                let cookies = $0.filter {
-                    $0.domain.contains(".instagram.com") && ["ds_user_id", "sessionid", "csrftoken"].contains($0.name)
-                }.sorted { $0.name < $1.name }
-                // Prepare `Secret` or do nothing.
-                guard cookies.count == 3 else { return }
-                webView.navigationDelegate = nil
-                self.onChange?(.success(.init(identifier: cookies[1],
-                                         crossSiteRequestForgery: cookies[0],
-                                         session: cookies[2])))
-                // Do not notify again.
-                self.onChange = nil
-            }
-        }
-    }
-}
-
 @available(iOS 11.0, macOS 10.13, *)
 /**
    A `class` describing an `Authenticator` relying on a `WKWebView` to fetch cookies.
@@ -86,7 +40,7 @@ internal final class WebView: WKWebView, WKNavigationDelegate {
    }
    ```
 */
-public final class WebViewAuthenticator<Storage: Swiftagram.Storage>: NSObject, Authenticator, WKNavigationDelegate {
+public final class WebViewAuthenticator<Storage: Swiftagram.Storage>: Authenticator {
     /// A `Storage` instance used to store `Secret`s.
     public internal(set) var storage: Storage
     /// A block outputing a configured `WKWebView`.
@@ -140,8 +94,6 @@ public final class WebViewAuthenticator<Storage: Swiftagram.Storage>: NSObject, 
                                                     }
         }
     }
-
-    // MARK: Navigation
 }
 
 @available(iOS 11.0, macOS 10.13, *)
