49e66e4f1ae7136b7a2b0f57e13019e8755f4704
sbertix <stefano.bertagno@gmail.com>
2020-03-11 18:19:41 +0100

    Add pagination and refactor request flow

--- Sources/Swiftagram/Client/Requester/RequesterTask.swift
+++ Sources/Swiftagram/Client/Requester/RequesterTask.swift
@@ -2,40 +2,121 @@
 //  RequesterTask.swift
 //  Swiftagram
 //
-//  Created by Stefano Bertagno on 08/03/2020.
+//  Created by Stefano Bertagno on 11/03/2020.
 //
 
 import Foundation
 
 public extension Requester {
-    /// A `struct` holding reference to a specific `Request`.
+    /// A `class` holding reference to an endpoint fetching, pausable and cancellable, task.
     final class Task: Hashable {
-        /// A `tuple` holding reference to responses.
-        public typealias Response<Type> = (data: Type, response: HTTPURLResponse?)
-
-        /// A `String` representing the current `hashValue`.
-        internal var identifier: String = UUID().uuidString
-        /// A `Date` indicating when the `Request` was first resumed.
-        public internal(set) var startedAt: Date = .init()
-        /// An associated `Request`.
-        public internal(set) var request: Request
-
+        /// The response.
+        public typealias Response<Data> = (data: Data, response: HTTPURLResponse?)
+        /// The result.
+        public typealias Result<Data> = Swift.Result<Response<Data>, Swift.Error>
+        
+        /// The task identifier.
+        internal var identifier = UUID().uuidString
+        
+        /// The originating `Endpoint`.
+        public let originating: Endpoint
+        /// The current `Endpoint`.
+        public var current: Endpoint?
+        
+        /// A weak reference to the `Requester`.
+        public weak var requester: Requester?
+        /// The current `URLSessionDataTask`.
+        internal var sessionTask: URLSessionDataTask?
+        /// A block requesting the next `Endpoint`.
+        internal var next: (Result<Data>) -> Endpoint?
+        
         // MARK: Lifecycle
-        /// Cancel on `deinit`.
+        /// Deinit.
         deinit { cancel() }
-
+        
         /// Init.
-        /// - parameter request: A valid `Request`.
-        public init(request: Request) { self.request = request }
-
-        /// Cancel the current request.
-        public func cancel() {
-            request.task?.cancel()
-            request.requester?.requests.remove(self)
+        /// - parameters:
+        ///     - endpoint: The originating `Endpoint`.
+        ///     - requester: A valid `Requester`. Defaults to `.default`.
+        ///     - next: A block outputting the last response and requesting the following `Endpoint`. `nil` to stop.
+        internal init(endpoint: Endpoint,
+                      requester: Requester = .default,
+                      next: @escaping (Result<Data>) -> Endpoint?) {
+            self.originating = endpoint
+            self.current = endpoint
+            self.requester = requester
+            self.next = next
         }
-
+        
+        // MARK: Handling
+        /// Cancel the current and all future requests.
+        public func cancel() {
+            self.requester?.cancel(self)
+            self.sessionTask?.cancel()
+            self.sessionTask = nil
+            self.current = nil
+        }
+        
+        /// Cancel the current request.
+        public func pause() {
+            self.sessionTask?.cancel()
+            self.sessionTask = nil
+        }
+        
+        /// Fetch the `current` endpoint.
+        /// - returns: `true` if there are no active tasks, `Endpoint` was valid and `requester` was not deallocated, `false` otherwise.
+        @discardableResult
+        public func resume() -> Bool {
+            guard sessionTask == nil,
+                let endpoint = current,
+                endpoint.request() != nil,
+                let requester = requester else {
+                    return false
+            }
+            /// Add to the `requester`.
+            requester.schedule(self)
+            return true
+        }
+        
+        // MARK: Fetching
+        /// Fetch using a given `session`.
+        /// - parameters:
+        ///     - session: A `URLSession`.
+        ///     -  configuration: A `Requester.Configuration`.
+        internal func fetch(using session: URLSession,
+                            configuration: Requester.Configuration) {
+            // Check for a valid `URL`.
+            guard let request = current?.request() else {
+                configuration.mapQueue.handle { [weak self] in
+                    self?.current = self?.next(.failure(Error.invalidEndpoint))
+                    self?.pause()
+                    configuration.requestQueue.handle { self?.resume() }
+                }
+                return
+            }
+            // Set `task`.
+            configuration.requestQueue.handle {
+                self.sessionTask = session.dataTask(with: request) { [weak self] data, response, error in
+                    configuration.mapQueue.handle {
+                        if let error = error {
+                            self?.current = self?.next(.failure(error))
+                        } else if let data = data {
+                            self?.current = self?.next(.success((data, response as? HTTPURLResponse)))
+                        } else {
+                            self?.current = self?.next(.failure(Error.invalidData))
+                        }
+                        self?.pause()
+                        configuration.requestQueue.handle { self?.resume() }
+                    }
+                }
+                self.sessionTask?.resume()
+            }
+        }
+        
         // MARK: Hashable
+        /// Conform to hashable.
         public func hash(into hasher: inout Hasher) { hasher.combine(identifier) }
+        /// Conform to equatable.
         public static func ==(lhs: Task, rhs: Task) -> Bool { return lhs.identifier == rhs.identifier }
     }
 }
--- Tests/SwiftagramTests/SwiftagramEndpointTests.swift
+++ Tests/SwiftagramTests/SwiftagramEndpointTests.swift
@@ -14,7 +14,7 @@ final class SwiftagramEndpointTests: XCTestCase {
     func testEndpointArchive() {
         XCTAssert(Endpoint.Archive
             .stories
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/archive/reel/day_shells/")
     }
@@ -23,12 +23,12 @@ final class SwiftagramEndpointTests: XCTestCase {
     func testEndpointDirect() {
         XCTAssert(Endpoint.Direct
             .threads
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/direct_v2/reel/inbox/")
         XCTAssert(Endpoint.Direct
             .thread(matching: "id")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/direct_v2/threads/id/")
     }
@@ -37,37 +37,37 @@ final class SwiftagramEndpointTests: XCTestCase {
     func testEndpointFeed() {
         XCTAssert(Endpoint.Feed
             .followedStories
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/feed/reels_tray/")
         XCTAssert(Endpoint.Feed
             .likes
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/feed/liked/")
         XCTAssert(Endpoint.Feed
             .timeline
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/feed/timeline/")
         XCTAssert(Endpoint.Feed
             .posts(by: "id")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/feed/user/id/")
         XCTAssert(Endpoint.Feed
             .stories(by: "id")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/feed/user/id/reel_media/")
         XCTAssert(Endpoint.Feed
             .posts(including: "id")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/usertags/id/feed/")
         XCTAssert(Endpoint.Feed
             .tagged(with: "tag")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/feed/tag/tag/")
     }
@@ -76,17 +76,17 @@ final class SwiftagramEndpointTests: XCTestCase {
     func testEndpointFriendship() {
         XCTAssert(Endpoint.Friendship
             .followed(by: "id")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/friendships/id/following/")
         XCTAssert(Endpoint.Friendship
             .following("id")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/friendships/id/followers/")
         XCTAssert(Endpoint.Friendship
             .friendship(with: "id")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/friendships/show/id/")
     }
@@ -95,14 +95,59 @@ final class SwiftagramEndpointTests: XCTestCase {
     func testEndpointUser() {
         XCTAssert(Endpoint.User
             .summary(for: "id")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/users/id/info/")
         XCTAssert(Endpoint.User
             .all(matching: "query")
-            .request?
+            .request()?
             .url?
             .absoluteString == "https://i.instagram.com/api/v1/users/search/?q=query")
+    }
+    
+    /// Test pagination.
+    func testPaginationString() {
+        // The current offset.
+        let expectation = XCTestExpectation()
+        var offset = -1
+        let languages = ["de", "it", "fr"]
+        // Paginate.
+        Endpoint.generic
+            .cycleTask(String.self,
+                       key: "l",
+                       initial: "en",
+                       next: { _ in offset += 1; return offset < languages.count ? languages[offset] : nil }) {
+                        switch $0 {
+                        case .success: break
+                        case .failure(let error): XCTFail(error.localizedDescription)
+                        }
+                        // Finish on the last one.
+                        if offset == 2 { expectation.fulfill() }
+            }
+            .resume()
+        wait(for: [expectation], timeout: 30)
+    }
+    
+    /// Test pagination.
+    func testPaginationResponse() {
+        // The current offset.
+        let expectation = XCTestExpectation()
+        var offset = -1
+        let languages = ["de", "it", "fr"]
+        // Paginate.
+        Endpoint.generic
+            .cycleTask(key: "l",
+                       initial: "en",
+                       next: { _ in offset += 1; return offset < languages.count ? languages[offset] : nil }) {
+                        switch $0 {
+                        case .success: break
+                        case .failure(let error): XCTFail(error.localizedDescription)
+                        }
+                        // Finish on the last one.
+                        if offset == 2 { expectation.fulfill() }
+            }
+            .resume()
+        wait(for: [expectation], timeout: 30)
     }
 
     static var allTests = [
@@ -111,6 +156,8 @@ final class SwiftagramEndpointTests: XCTestCase {
         ("Endpoint.Direct", testEndpointDirect),
         ("Endpoint.Feed", testEndpointFeed),
         ("Endpoint.Friendship", testEndpointFriendship),
-        ("Endpoint.User", testEndpointUser)
+        ("Endpoint.User", testEndpointUser),
+        ("Endpoint.Pagination.String", testPaginationString),
+        ("Endpoint.Pagination.Response", testPaginationResponse),
     ]
 }
