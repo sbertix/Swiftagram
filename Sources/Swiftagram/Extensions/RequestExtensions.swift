28e1f50bc6731bc9be6893e4b4cc509ad2320399
sbertix <stefano.bertagno@gmail.com>
2020-04-19 16:10:40 +0200

    Update `Endpoint.Feed.timeline`

--- Sources/Swiftagram/Extensions/RequestExtensions.swift
+++ Sources/Swiftagram/Extensions/RequestExtensions.swift
@@ -13,13 +13,35 @@ import ComposableRequest
 public extension Composable {
     /// Append to `headerFields`.
     func defaultHeader() -> Self {
-        return header(
-            ["Accept-Language": "en-US",
-             "Content-Type": "application/x-www-form-urlencoded",
-             "X-IG-Capabilities": "3brTvw==",
-             "X-IG-Connection-Type": "WIFI",
-             "User-Agent": Device.default.apiUserAgent]
-        )
+        return header([
+            "User-Agent": Device.default.apiUserAgent,
+            "X-Ads-Opt-Out": "0",
+            "X-CM-Bandwidth-KBPS": "-1.000",
+            "X-CM-Latency": "-1.000",
+            "X-IG-App-Locale": "en_US",
+            "X-IG-Device-Locale": "en_US",
+            "X-Pigeon-Session-Id": UUID().uuidString,
+            "X-Pigeon-Rawclienttime": "\(Int(Date().timeIntervalSince1970/1000)).000",
+            "X-IG-Connection-Speed": "\(Int.random(in: 1000...3700))kbps",
+            "X-IG-Bandwidth-Speed-KBPS": "-1.000",
+            "X-IG-Bandwidth-TotalBytes-B": "0",
+            "X-IG-Bandwidth-TotalTime-MS": "0",
+            "X-IG-EU-DC-ENABLED": "0",
+            "X-IG-Extended-CDN-Thumbnail-Cache-Busting-Value": "1000",
+            "X-Bloks-Version-Id": "1b030ce63a06c25f3e4de6aaaf6802fe1e76401bc5ab6e5fb85ed6c2d333e0c7",
+            "X-IG-WWW-Claim": "0",
+            "X-Bloks-Is-Layout-RTL": "false",
+            "X-IG-Connection-Type": "WIFI",
+            "X-IG-Capabilities": "3brTvwE=",
+            "X-IG-App-ID": "567067343352427",
+            "X-IG-Device-ID": Device.default.deviceGUID.uuidString,
+            "X-IG-Android-ID": Device.default.deviceIdentifier,
+            "Accept-Language": "en-US",
+            "X-FB-HTTP-Engine": "Liger",
+            "Host": "i.instagram.com",
+            "Accept-Encoding": "gzip",
+            "Connection": "close"
+        ])
     }
 }
 
