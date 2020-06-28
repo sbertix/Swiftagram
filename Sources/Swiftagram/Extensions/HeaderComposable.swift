894b11785a798805637a192254a3ec31196bbd48
sbertix <stefano.bertagno@gmail.com>
2020-06-28 20:38:30 +0200

    Update default headers

--- Sources/Swiftagram/Extensions/HeaderComposable.swift
+++ Sources/Swiftagram/Extensions/HeaderComposable.swift
@@ -22,28 +22,25 @@ public extension HeaderComposable where Self: HeaderParsable {
             "X-IG-App-Locale": "en_US",
             "X-IG-Device-Locale": "en_US",
             "X-Pigeon-Session-Id": UUID().uuidString.lowercased(),
-            "X-Pigeon-Rawclienttime": "\(Int(Date().timeIntervalSince1970/1000)).000",
+            "X-Pigeon-Rawclienttime": "\(Int(Date().timeIntervalSince1970)).000",
             "X-IG-Connection-Speed": "\(Int.random(in: 1000...3700))kbps",
             "X-IG-Bandwidth-Speed-KBPS": "-1.000",
             "X-IG-Bandwidth-TotalBytes-B": "0",
             "X-IG-Bandwidth-TotalTime-MS": "0",
-            "X-IG-EU-DC-ENABLED": "0",
             "X-IG-Extended-CDN-Thumbnail-Cache-Busting-Value": "1000",
-            "X-Bloks-Version-Id": Constants.bloksVersionId,
+            "X-Bloks-Version-Id": Constants.bloksVersionId.lowercased(),
             "X-IG-WWW-Claim": "0",
             "X-Bloks-Is-Layout-RTL": "false",
             "X-IG-Connection-Type": "WIFI",
-            "X-IG-Capabilities": "3brTvw==",
+            "X-IG-Capabilities": "3brTvwE=",
             "X-IG-App-ID": "567067343352427",
             "X-IG-Device-ID": Device.default.deviceGUID.uuidString.lowercased(),
             "X-IG-Android-ID": Device.default.deviceIdentifier.lowercased(),
             "Accept-Language": "en-US",
             "X-FB-HTTP-Engine": "Liger",
             "Host": "i.instagram.com",
-            "Referer": "i.instagram.com",
-            "Origin": "i.instagram.com",
-            "Connection": "close",
-            "Content-Type": "application/x-www-form-urlencoded"
+            "Accept-Encoding": "gzip",
+            "Connection": "close"
         ])
     }
 }
