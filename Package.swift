5cac494387f450013df3aefaab219980f6559af1
sbertix <stefano.bertagno@gmail.com>
2020-06-28 19:59:36 +0200

    Roll back to `CryptoSwift`

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/evgenyneu/keychain-swift.git", .upToNextMinor(from: "19.0.0")),
-        .package(url: "https://github.com/soyersoyer/SwCrypt", .upToNextMinor(from: "5.1.0")),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", .upToNextMinor(from: "1.3.1")),
         .package(url: "https://github.com/sbertix/ComposableRequest", .upToNextMinor(from: "4.0.0")),
     ],
     targets: [
         .target(
             name: "Swiftagram",
-            dependencies: ["ComposableRequest", "SwCrypt"]
+            dependencies: ["ComposableRequest", "CryptoSwift"]
         ),
         .target(
             name: "SwiftagramKeychain",
