4e11b0d241b915eae8058ca561400ee557241d83
sbertix <stefano@bertagno.com>
2021-05-22 12:06:13 +0200

    feat(endpoints): support delay between pages

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
                .library(name: "SwiftagramCrypto",
                         targets: ["SwiftagramCrypto"])],
     // Package dependencies.
-    dependencies: [.package(url: "https://github.com/sbertix/ComposableRequest", .upToNextMinor(from: "5.1.0")),
+    dependencies: [.package(url: "https://github.com/sbertix/ComposableRequest", .upToNextMinor(from: "5.2.1")),
                    .package(url: "https://github.com/sbertix/SwCrypt.git", .upToNextMinor(from: "5.1.0"))],
     // All targets.
     targets: [.target(name: "Swiftagram",
