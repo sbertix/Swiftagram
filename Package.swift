2a5e7d5258172aa8825740227b6637f60a936b86
Stefano Bertagno <stefano@bertagno.com>
2021-08-27 23:33:34 +0200

    feat: lower minimum platform versions

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ import PackageDescription
 let package = Package(
     name: "Swiftagram",
     // Supported versions.
-    platforms: [.iOS("13.0"),
-                .macOS("10.15"),
-                .tvOS("13.0"),
-                .watchOS("6.0")],
+    platforms: [.iOS("12.0"),
+                .macOS("10.14"),
+                .tvOS("12.0"),
+                .watchOS("5.0")],
     // Exposed libraries.
     products: [.library(name: "Swiftagram",
                         targets: ["Swiftagram"]),
